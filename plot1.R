172eae71e90293d83d64a4b9809f248a8e156f6d
Anne Mason <la-animaux@users.noreply.github.com>
2015-12-09 21:46:52 -0800

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,18 @@
+##This code creates the first plot for Course Project 1
+
+##Reading the data
+rawdata <- read.table("household_power_consumption.txt", sep=";", header = TRUE)
+powerCons <- rawdata[ which(rawdata$Date=="1/2/2007" | rawdata$Date=="2/2/2007"), ]
+remove(rawdata)
+
+##Clean up the data
+powerCons$Date <- as.Date(powerCons$Date, "%d/%m/%Y")
+powerCons$DateTime <- paste(powerCons$Date, " ", powerCons$Time)
+powerCons$DateTime <- as.POSIXct(powerCons$DateTime)
+
+##Load the library
+library(datasets)
+
+##Draw the histogram
+powerCons$Global_active_power <-as.numeric(as.character(powerCons$Global_active_power))
+with(powerCons, hist(Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)"))
