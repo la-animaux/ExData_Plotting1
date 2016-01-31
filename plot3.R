19aebe3b19a5400db249df332c67c1355193f48b
Anne Mason <la-animaux@users.noreply.github.com>
2016-01-31 11:36:54 -0800

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -17,6 +17,7 @@ powerCons$Sub_metering_3 <-as.numeric(as.character(powerCons$Sub_metering_3))
 library(datasets)
 
 ##Draw the line chart
+png(filename="plot3.png", width=480, height=480)
 plot(powerCons$Sub_metering_1, type="l", ylim=range(powerCons$Sub_metering_1), ann=T, xaxt='n', xlab=" ", ylab="Energy sub metering", cex.lab=0.8, cex=0.8)  #draw the plot and the first line
 axis(1, at=1, lab=c("Thu")) #label the x-axis
 axis(1, at=1439, lab=c("Fri")) #label the x-axis
@@ -24,3 +25,4 @@ axis(1, at=2879, lab=c("Sat")) #label the x-axis
 lines(powerCons$Sub_metering_2, type="l", col="red")   #add the second line for Sub_metering_2
 lines(powerCons$Sub_metering_3, type="l", col="blue")   #add the third line for Sub_metering_3
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=c(1,1), col=c("black", "red", "blue"), cex=0.8)
+dev.off()
